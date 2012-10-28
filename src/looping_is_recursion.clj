fbd930c3e2def63e3739f5ab2f41849d3b8672fc
Janne Inkinen <inkinen.janne@gmail.com>
2012-10-28 23:07:33 +0200

    rekursio

--- src/looping_is_recursion.clj
+++ src/looping_is_recursion.clj
@@ -1,19 +1,42 @@
 (ns looping-is-recursion)
 
-(defn power [base exp]
-  ":(")
+; Harjoitus 1
+; J‰i kesken, Math/Pow:illa olisi onnistunut, mutta
+; sit‰ ei varmaan ollut tarkoitus k‰ytt‰‰
+(defn power
+        [base exp]
+  (let [helper (fn [acc n]
+                 (if (zero? n)
+                   acc
+                   (recur (* acc acc) (dec n))))]
+    (helper base exp))
+)
 
-(defn last-element [a-seq]
-  ":(")
+; Harjoitus 2
+(defn last-element
+        [a-seq]
+        (last a-seq)
+)
 
-(defn seq= [seq1 seq2]
-  ":(")
+; Harjoitus 3
+(defn seq=
+        [seq1 seq2]
+        (if (= seq1 seq2)
+                true
+                false
+        )
+)
 
+; Harjoitus 4
 (defn find-first-index [pred a-seq]
   ":(")
 
-(defn avg [a-seq]
-  -1)
+; Harjoitus 5
+(defn avg
+        [a-seq]
+        (/ (apply + a-seq) (count a-seq))
+)
+
 
 (defn parity [a-seq]
   ":(")
